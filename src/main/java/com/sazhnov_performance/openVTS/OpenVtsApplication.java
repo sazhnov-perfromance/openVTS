b922ea90c490a86711767f96564b2bb2222608c1
sazhnov-perfromance <sajnov.mv+sazhnov-performance@gmail.com>
2025-03-12 18:25:55 -0400

    Update OpenVtsApplication.java

--- src/main/java/com/sazhnov_performance/openVTS/OpenVtsApplication.java
+++ src/main/java/com/sazhnov_performance/openVTS/OpenVtsApplication.java
@@ -3,11 +3,23 @@ package com.sazhnov_performance.openVTS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the OpenVTS application.
+ */
 @SpringBootApplication
-public class OpenVtsApplication {
+public final class OpenVtsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenVtsApplication.class, args);
-	}
+    // Private constructor to prevent instantiation
+    private OpenVtsApplication() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 
+    /**
+     * Main entry point of the application.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(OpenVtsApplication.class, args);
+    }
 }
